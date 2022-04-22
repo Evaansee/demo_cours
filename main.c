7ab258f3c1eea8668144bb178794bee19051afd6
evaansee <ean@cy-tech.fr>
2022-04-22 08:55:49 +0200

    mon premier commit

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
-	#include <stdio.h>
+#include <stdio.h>
 
 int main(){
 	printf("Bonjour le monde  ! \n");
-	printf(" linfo c'est trop bien !");
-	printf("suis je sur le serveur?");
-	int a=10;
-	
 	return 0;
 
 }
